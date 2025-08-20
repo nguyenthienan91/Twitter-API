a03954d99cd8c94f66dca2f8fca1207bddc014e7
Nguyen Thien An <nguyenthienandzpro@gmail.com>
2025-08-20 11:21:22 +0700

    connect to mongo atlas by mongodb driver

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express'
 import usersRouter from './routes/users.route'
+import databaseService from './services/database.services'
 const app = express()
 
 const PORT = 3000
 
 // thêm middleware này để parse JSON
 app.use(express.json())
-
 app.use('/users', usersRouter)
+
+databaseService.connect()
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
