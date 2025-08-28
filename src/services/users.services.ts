6c41f0145009a1d87351148c155669c070f049d9
Nguyen Thien An <nguyenthienandzpro@gmail.com>
2025-08-28 09:05:23 +0700

    check exist email

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -2,7 +2,7 @@ import User from '~/models/schemas/User.schema'
 import databaseService from './database.services'
 
 class UsersService {
-  async register(payload: { email: string, password: string }) {
+  async register(payload: { email: string; password: string }) {
     const { email, password } = payload
     const res = await databaseService.users.insertOne(
       new User({
@@ -11,6 +11,12 @@ class UsersService {
       })
     )
     return res
+  }
+
+  async checkEmailExist(email: string) {
+    const user = databaseService.users.findOne({ email })
+    console.log(user)
+    return Boolean(user)
   }
 }
 
