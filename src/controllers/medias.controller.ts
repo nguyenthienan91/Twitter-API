f0bac862ae66b631296fafb394561fd60386bae6
Nguyen Thien An <nguyenthienandzpro@gmail.com>
2025-09-16 10:54:05 +0700

    upload file with formidable library

---
+++ src/controllers/medias.controller.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from 'express'
+import path from 'path'
+import { handleUploadSingleImage } from '~/utils/file'
+// console.log(__dirname)
+// console.log(path.resolve('uploads'))
+
+export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
+  const data = await handleUploadSingleImage(req)
+  return res.json({
+    result: data
+  })
+}
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,20 @@ import express from 'express'
 import usersRouter from './routes/users.route'
 import databaseService from './services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
+import mediasRouter from './routes/medias.route'
+import { initFolder } from './utils/file'
 
 databaseService.connect()
 const app = express()
 const PORT = 4000
 
+// Tạo folder upload nếu chưa tồn tại
+initFolder()
+
 // thêm middleware này để parse JSON
 app.use(express.json())
 app.use('/users', usersRouter)
+app.use('/medias', mediasRouter)
 app.use(defaultErrorHandler) //default handler
 
 app.listen(PORT, () => {
