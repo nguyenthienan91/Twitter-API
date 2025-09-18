95a0c49724e8f0396067bd9b41a07176eceb01d1
Nguyen Thien An <nguyenthienandzpro@gmail.com>
2025-09-18 11:37:19 +0700

    another way to serve static file

--- src/controllers/medias.controller.ts
+++ src/controllers/medias.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
 import path from 'path'
+import { UPLOAD_DIR } from '~/constants/directory'
 import { USERS_MESSAGES } from '~/constants/messages'
 import mediasService from '~/services/medias.services'
 import { handleUploadSingleImage } from '~/utils/file'
@@ -11,5 +12,15 @@ export const uploadSingleImageController = async (req: Request, res: Response, n
   return res.json({
     message: USERS_MESSAGES.UPLOAD_IMAGE_SUCCESS,
     result: url
+  })
+}
+
+export const serveImageController = async (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.params
+  console.log(name)
+  return res.sendFile(path.resolve(UPLOAD_DIR, name), (err) => {
+    if (err) {
+      res.status((err as any).status).send('Not Found')
+    }
   })
 }
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import mediasRouter from './routes/medias.route'
 import { initFolder } from './utils/file'
 import { config } from 'dotenv'
 import { UPLOAD_DIR } from './constants/directory'
+import staticRouter from './routes/static.routes'
 
 config()
 
@@ -16,14 +17,16 @@ const PORT = process.env.PORT || 4000
 // Tạo folder upload nếu chưa tồn tại
 initFolder()
 // Xử lí serving static file
-app.use('/static', express.static(UPLOAD_DIR))
+// app.use('/static', express.static(UPLOAD_DIR))
 
 // thêm middleware này để parse JSON
 app.use(express.json())
 app.use('/users', usersRouter)
 app.use('/medias', mediasRouter)
-app.use(defaultErrorHandler) //default handler
+app.use('/static', staticRouter)
 
+
+app.use(defaultErrorHandler) //default handler
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
